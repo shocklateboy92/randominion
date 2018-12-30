e3b4c4d6b611c592dad21b000b5bdd31607235c1
Lasath Fernando <devel@lasath.org>
2018-12-29 17:44:46 -0800

    Update to react-scripts 4.0.8
    I'm pretty sure this is liek a pre-alpha version, but it lets me use Sass so...

--- src/containers/cards-list.tsx
+++ src/containers/cards-list.tsx
@@ -3,6 +3,8 @@ import { Card } from 'src/cards';
 import { Dictionary } from 'src/models/dictionary';
 import CardDisplay from 'src/components/card-display';
 
+import '../styles/cards-list.scss';
+
 interface ICardsListState {
     imageUrls: Dictionary<string>;
 }
@@ -21,33 +23,18 @@ export default class CardsList extends React.Component<
         this.state = { imageUrls: {} };
     }
 
-    public async componentDidMount() {
-        const response = await fetch(
-            `http://wiki.dominionstrategy.com/api.php?action=query&format=json&prop=imageinfo&titles=${this.props.cardsToDisplay
-                .map(card => `File:${card.name}`)
-                .join('|')}&iiprop=url`
-        );
-        const results = await response.json();
-        const urls = this.state.imageUrls;
-
-        for (const info of Object.values<any>(results.query.pages)) {
-            const card = info.title
-                .replace(/^File:/, '')
-                .replace(/\.jpg$/, '')
-                .replace('_', ' ');
-            urls[card] = info.imageinfo && info.imageinfo.url;
-        }
-
-        this.setState({ imageUrls: urls });
-    }
-
     public render() {
-        return this.props.cardsToDisplay.map(card => (
-            <CardDisplay
-                key={card.name}
-                card={card}
-                imageUrl={this.state.imageUrls[card.name]}
-            />
-        ));
+        return (
+            <div className="cards-list">
+                {' '}
+                {this.props.cardsToDisplay.map(card => (
+                    <CardDisplay
+                        key={card.name}
+                        card={card}
+                        imageUrl={this.state.imageUrls[card.name]}
+                    />
+                ))}
+            </div>
+        );
     }
 }
