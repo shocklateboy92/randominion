143ccc661fb9ebe6c02d42cfeb9dcc961f890ded
Lasath Fernando <devel@lasath.org>
2019-01-03 22:36:05 -0800

    Updates

---
+++ precache-manifest.ec2241dad3895c0befefcd5037ab98b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5eac62fdcb6d74bf708",
+    "url": "/randominion/static/css/main.af5f25a6.chunk.css"
+  },
+  {
+    "revision": "b5eac62fdcb6d74bf708",
+    "url": "/randominion/static/js/main.b5eac62f.chunk.js"
+  },
+  {
+    "revision": "509749ac880a47103ecb",
+    "url": "/randominion/static/js/1.509749ac.chunk.js"
+  },
+  {
+    "revision": "370f804272b508f9db02",
+    "url": "/randominion/static/js/runtime~main.370f8042.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randominion/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b11f35f77210c651367969365f87af24",
+    "url": "/randominion/static/media/trajan-pro-regular.b11f35f7.ttf"
+  },
+  {
+    "revision": "16f86dfce9d44adc6311d2ea9ed86bb5",
+    "url": "/randominion/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/randominion/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/randominion/workbox-v3.6.3"});
+
+importScripts(
+  "/randominion/precache-manifest.ec2241dad3895c0befefcd5037ab98b1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/randominion/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
