0f8959dea25f1178d0d69b7be01469f5af3b7f6f
Andres Chacon <anchac@microsoft.com>
2019-01-06 22:09:57 -0800

    wip

--- src/containers/randomizer.tsx
+++ src/containers/randomizer.tsx
@@ -23,14 +23,14 @@ interface IRandomizerProps {
 const Component: React.SFC<IRandomizerProps> = props => (
     <div className='randomizer'>
         <div className='toolbar'>
-            <div className='btn-container btn btn-one' onClick={props.randomize}><span>Randomize</span></div>
-            <div className='btn-container btn btn-one' onClick={props.unlockAll}><span>Unlock All</span></div>
-            <div className={classNames('btn-container btn btn-one', { 'disabled': !props.canUndo })} onClick={props.undo}>
+            <span className='btn-container btn btn-one' onClick={props.randomize}><span>Randomize</span></span>
+            <span className='btn-container btn btn-one' onClick={props.unlockAll}><span>Unlock All</span></span>
+            <span className={classNames('btn-container btn btn-one', { 'disabled': !props.canUndo })} onClick={props.undo}>
                 <span>Undo</span>
-            </div>
-            <div className={classNames('btn-container btn btn-one', { 'disabled': !props.canRedo })} onClick={props.redo}>
+            </span>
+            <span className={classNames('btn-container btn btn-one', { 'disabled': !props.canRedo })} onClick={props.redo}>
                 <span>Redo</span>
-            </div>
+            </span>
         </div>
         <div className='container'>
             <CardsList />
